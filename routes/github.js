7e6e095089eb61be970224f0ffdef1b8058525c1
Rawan <rawan.nujoom@gmail.com>
2020-06-15 23:08:38 +0300

    lint

--- routes/github.js
+++ routes/github.js
@@ -7,29 +7,30 @@ require('dotenv').config();
 const router = express.Router();
 
 const endpoints = {
-    base: 'https://api.github.com',
-    usersUrl: '/search/users'
+  base: 'https://api.github.com',
+  usersUrl: '/search/users',
 };
 router.get('/user', (req, res, next)=> {
-    let query = req.query.keyword;
-    let page = req.query.page || 1;
-    if (query && query.trim()) {
-        let token = process.env.USER_SEARCH_OAUTH || ''; 
-        token = token ? `token ${token}`: '';
-        superagent
-            .get('https://api.github.com/search/users')
-            .query({ q: query })
-            .query({ page: page })
-            .set('User-Agent', 'rawan-nujoom/1.0')
-            .set('Authorization', token)
-            .then(result => {
-                res.status(200).json(result.body);
-            }).catch(err=> {
-                next(err);
-            });
-    } else {
-        res.status(500).send({err: 'Empty Search'})
-    }
+  const url = endpoints.base + endpoints.usersUrl;
+  const query = req.query.keyword;
+  const page = req.query.page || 1;
+  if (query && query.trim()) {
+    let token = process.env.USER_SEARCH_OAUTH || ''; 
+    token = token ? `token ${token}`: '';
+    superagent
+      .get(url)
+      .query({ q: query })
+      .query({ page: page })
+      .set('User-Agent', 'rawan-nujoom/1.0')
+      .set('Authorization', token)
+      .then(result => {
+        res.status(200).json(result.body);
+      }).catch(err=> {
+        next(err);
+      });
+  } else {
+    res.status(500).send({err: 'Empty Search'});
+  }
 });
 
 module.exports = router;
